1ac643f2952a328a2c5b7046f57b01c420414809
Valentina Volgina <64236396+valentinavolgina2@users.noreply.github.com>
2025-01-20 14:05:16 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []); // only do it once when we load it first time
+
+  const toggleWatchlist = (movieId) => {
+    setWatchlist((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  }; // prev - previous state which is an array of movies ids. States are immutable -> that's why we copy prev array using spread operator
+
+  return (
+    <div className="App">
+      <div className="container">
+        <Header></Header>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            ></Route>
+            <Route
+              path="/watchlist"
+              element={
+                <Watchlist
+                  watchlist={watchlist}
+                  movies={movies}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
